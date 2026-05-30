a76aa5212c069acf0cc0e783fd26df63fe42b6d6
Diego Melgoza <drmelgoza@icloud.com>
2026-05-30 00:03:16 -0700

    implement food item patch endpoint

--- src/api/users.py
+++ src/api/users.py
@@ -370,11 +370,20 @@ class GoalCategory(str, Enum):
     calories = "calories"
 
 
-@router.patch("/{user_id}/goals/{goal}", response_model=GoalCreateResponse)
+class GoalUpdateResponse(BaseModel):
+    user_id: int
+    status: str
+
+
+@router.patch("/{user_id}/goals/{goal}", response_model=GoalUpdateResponse)
 def update_macro_goal(user_id: int, goal: GoalCategory, quantity: int = 1):
     valid_user = validate_user(user_id)
     if not valid_user:
         raise HTTPException(status_code=404, detail="User does not exist.")
+
+    valid_goal = validate_goal(user_id, goal.value)
+    if not valid_goal:
+        raise HTTPException(status_code=400, detail="Goal does not exist.")
 
     with db.engine.begin() as connection:
         search_result = connection.execute(
@@ -394,9 +403,9 @@ def update_macro_goal(user_id: int, goal: GoalCategory, quantity: int = 1):
             }]
         ).one_or_none()
 
-        status = "updated" if search_result else "goal does not exist."
+        status = "updated" if search_result else "error; please try again."
 
-        return GoalCreateResponse(user_id=user_id, status=status)
+        return GoalUpdateResponse(user_id=user_id, status=status)
 
 
 
@@ -542,8 +551,61 @@ def get_food_items(user_id: int):
 
         return ItemGetResponse(user_id=user_id, items=items)
 
+class ItemPatchResponse(BaseModel):
+    user_id: int
+    item_id: int
+    status: str
 
 
+@router.patch("/{user_id}/items/{item_id}", response_model=ItemPatchResponse)
+def update_food_item(
+    user_id: int,
+    item_id: int,
+    new_name: str = None,
+    new_calories: float = None,
+    new_protein: float = None,
+    new_carbs: float = None,
+    new_fat: float = None,
+):
+
+    valid_user = validate_user(user_id)
+    if not valid_user:
+        raise HTTPException(status_code=404, detail="User does not exist.")
+
+    valid_item = validate_item(item_id)
+    if not valid_item:
+        raise HTTPException(status_code=404, detail="Item does not exist.")
+
+    metadata = sqlalchemy.MetaData()
+    user_items = sqlalchemy.Table("user_items", metadata, autoload_with=db.engine)
+
+    query = (
+        sqlalchemy.update(user_items)
+        .where(user_items.c.id == item_id)
+        .where(user_items.c.user_id == user_id)
+    )
+
+    if new_name:
+        query = query.values(name=new_name)
+
+    if new_calories:
+        query = query.values(calories=new_calories)
+
+    if new_protein:
+        query = query.values(protein=new_protein)
+
+    if new_carbs:
+        query = query.values(carbs=new_carbs)
+
+    if new_fat:
+        query = query.values(fat=new_fat)
+
+    with db.engine.begin() as connection:
+        result = connection.execute(query.returning(1)).one_or_none()
+
+        status = "updated" if result else "error; please try again"
+
+    return ItemPatchResponse(user_id=user_id, item_id=item_id, status=status)
 
 class ItemDeleteResponse(BaseModel):
     user_id: int
